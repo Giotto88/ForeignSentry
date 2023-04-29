d3d7a9532c4d8601660282885c10e41c129fbe6f
Franz Ricci <franzricci88@gmail.com>
2023-04-29 15:20:03 +0200

    First unban command

--- src/main/java/online/darklounge/foreignsentry/ForeignSentry.java
+++ src/main/java/online/darklounge/foreignsentry/ForeignSentry.java
@@ -1,9 +1,16 @@
 package online.darklounge.foreignsentry;
 
+import online.darklounge.foreignsentry.commands.psk;
 import online.darklounge.foreignsentry.handler.ConnectionHandler;
+//import online.darklounge.foreignsentry.commands.Secretasked;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import org.bukkit.ChatColor;
 import org.bukkit.event.Listener;
+
+import java.util.logging.Level;
 
 public final class ForeignSentry extends JavaPlugin implements Listener {
 
@@ -13,12 +20,21 @@ public final class ForeignSentry extends JavaPlugin implements Listener {
         getLogger().info("Hello world");
 
         new ConnectionHandler(this);
+
+        // Register our command "kit" (set an instance of your command class as executor)
+        this.getCommand("psk").setExecutor(new psk());
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
         getLogger().info("Goodby world");
+    }
+
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
+        getLogger().info(sender.getName()+"ha tirato il comando: "+command.getName());
+        return true;
+        //return super.onCommand(sender, command, label, args);
     }
 
 
---
+++ src/main/java/online/darklounge/foreignsentry/commands/psk.java
@@ -0,0 +1,31 @@
+package online.darklounge.foreignsentry.commands;
+
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Objects;
+
+import static org.bukkit.Bukkit.getLogger;
+
+public class psk implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if( sender instanceof Player){
+            Player player = (Player) sender;
+            String ip = Objects.requireNonNull(player.getAddress()).getAddress().getHostAddress();
+
+            ItemStack piccone = new ItemStack(Material.STONE_SWORD,1);
+
+            player.getInventory().addItem(piccone);
+            Bukkit.unbanIP(ip);
+            getLogger().info("IP unbanned");
+        }
+        return true;
+    }
+}
